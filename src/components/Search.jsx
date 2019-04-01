97354e85040e07e1c5ff7ece1cebdf09c404b7b9
WAlex1978 <42897460+WAlex1978@users.noreply.github.com>
2019-03-31 20:36:32 -0700

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,7 @@ import Axios from 'axios';
 async function Search(props) {
     var value = props;
     var apiUrl = 'https://pixabay.com/api';
-    var apiKey = '12027914-00b10473d10ee790f2812000c';
+    var apiKey = ''; // Your PixaBay API Key here
     var images = [];
 
     var res = await Axios.get(`${apiUrl}/?key=${apiKey}&q=${value}&image_type=photo%order=latest&per_page=99`);
