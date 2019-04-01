c5818a372a9c12cfb8ad214cae59327099c12cf4
WAlex1978 <42897460+WAlex1978@users.noreply.github.com>
2019-03-31 23:58:24 -0700

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ async function Search(props) {
     var apiKey = ''; // Your PixaBay API Key here
     var images = [];
 
-    var res = await Axios.get(`${apiUrl}/?key=${apiKey}&q=${value}&image_type=photo%order=latest&per_page=198`);
+    var res = await Axios.get(`${apiUrl}/?key=${apiKey}&q=${value}&image_type=photo%order=latest&per_page=200`);
     images = res.data.hits;
     return images;
 }
